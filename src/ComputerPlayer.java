9d9314355a9faaf4d64b5d515b61de5636dcfea9
PaolaOtamendi <paola_otamendi23@hotmail.com>
2024-01-27 18:38:03 -0600

    fix cambios en guess de number for para recorrer los juegos

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,2 +1,16 @@
-public class ComputerPlayer {
+import java.util.Random;
+
+public class ComputerPlayer extends Player {
+    private Random random;
+
+    public ComputerPlayer(String name) {
+        super(name);
+        this.random = new Random();
+    }
+
+    public int makeGuess() {
+        int guess = random.nextInt(100) + 1;
+        getGuesses().add(guess);
+        return guess;
+    }
 }
--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -6,49 +6,52 @@ public class GuessTheNumberGame {
     private static int targetNumber;
 
     public static void main(String[] args) {
-        // Ejecuta el numero aleatorio y lo imprime en consola
-        targetNumber = random.nextInt(100) + 1;
-        System.out.println("Numero Aleatorio:" + targetNumber);
-
-        // Inicia el juego
         startGame();
     }
 
     public static void startGame() {
-        Player humanPlayer = new HumanPlayer("Player 1");
-        playGame(humanPlayer);
+        targetNumber = random.nextInt(100) + 1;
+        System.out.println("Numero Aleatorio:" + targetNumber);
+
+        Player humanPlayer = new HumanPlayer("Player 1 🤸‍♀️");
+        Player computerPlayer = new ComputerPlayer("Computer player 👩‍💻");
+
+        int maxGuesses = 5;
+        boolean gameFin = false;
+
+        for(int i = 0; i<maxGuesses; i++) {
+            playGame(humanPlayer);
+            if(checkGuess(humanPlayer)) {
+                break;
+            }
+
+            playGame(computerPlayer);
+            if(checkGuess(humanPlayer)) {
+                break;
+            }
+        }
+
+        if (!gameFin) {
+            System.out.println("🛑 Haz agotado tus juegos. El número correcto era: " + targetNumber);
+        }
     }
 
 
     private static void playGame(Player player) {
-        int maxGuesses = 5;
-        int guessesMade = 0;
+        int guess = player.makeGuess();
+        System.out.println("Juego 🎯: "+ player.getName() + " suposición: " + guess);
 
-
-        while (guessesMade < maxGuesses) {
-            int guess = player.makeGuess();
-            System.out.println("Juego de 🤹‍♂️: "+ player.getName() + " suposición: " + guess);
-            guessesMade++;
-
-            if(checkGuess(player, guess)) {
-                System.out.println(player.getName() + " ha adivinado el número. ¡Felicidades!");
-                break;
-            } else {
-                String result = (guess < targetNumber) ? "Muy baja 👎" : "Muy alta 🔥";
-                System.out.println("Tu resultado fue: " + result + ". Intenta nuevamente.\n");
-            }
-
-            /*checkGuess(player, guess);*/
-            System.out.println("Historial de suposiciones de " + player.getName() + ":" + player.getGuesses());
+        if(checkGuess(player)) {
+            System.out.println(player.getName() + " Haz adivinado el número. ¡Felicidades! 🎉");
+        } else {
+            String result = (guess < targetNumber) ? "muy baja 👎" : "muy alta 🔥";
+            System.out.println("La suposición fue " + result + ". Intenta nuevamente.\n");
         }
 
-        if (guessesMade == maxGuesses) {
-            System.out.println("Se han agotado las suposiciones. El número correcto era: " + targetNumber);
-        }
+        System.out.println("🚥 Historial de suposiciones de " + player.getName() + ": " + player.getGuesses() + "\n");
     }
 
-    private  static boolean checkGuess(Player player, int guess) {
-        return (guess == targetNumber);
+    private static boolean checkGuess(Player player) {
+        return player.getGuesses().contains(targetNumber);
     }
-
 }
